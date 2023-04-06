b49e5a8d09087078f0307ac9eca76884a7b6c2be
chinonso ata <chinonso.ata@gmail.com>
2023-04-06 01:23:10 +0100

    Feature: Set up auth dto and global pipes

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { AuthDto } from './dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
-  signup() {
+  signup(dto: AuthDto) {
     return 'Signup';
   }
 
-  signin() {
+  signin(dto: AuthDto) {
     return 'Signin';
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      // Ensures that we can't pass any extra values than what we need
+    })
+  );
   await app.listen(3000);
 }
 bootstrap();
