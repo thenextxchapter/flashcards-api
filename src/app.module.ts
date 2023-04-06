22ea7147abc25900f03d61e70246c9419fc9a7c5
chinonso ata <chinonso.ata@gmail.com>
2023-04-06 01:07:42 +0100

    Feature: Set up ground work for user authentication

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { FlashcardModule } from './flashcard/flashcard.module';
+import { AuthModule } from './auth/auth.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UserModule, FlashcardModule, AuthModule, PrismaModule],
 })
 export class AppModule {}
